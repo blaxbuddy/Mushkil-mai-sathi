8616a8b6c0754a014b3de53b569422f801e991ad
Raj Jat <68320015+blaxbuddy@users.noreply.github.com>
2025-11-29 08:57:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ $(document).ready(function(){
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
+        
         autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
--- scripts.js
+++ scripts.js
@@ -58,7 +58,8 @@ $(document).ready(function(){
         responsive: {
             0:{
                 items: 1,
-                nav: false
+                nav: true
+                
             },
             600:{
                 items: 2,
